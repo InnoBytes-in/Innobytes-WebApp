353e122973d59887206f2567132a217bcd8c84ed
Shaunak-Kayande <shaunakk.2801@gmail.com>
2023-02-23 11:57:30 +0530

    Work started
    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Cloud from "./Components/Cloud";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Cloud />
+    </>
+  );
+}
+
+export default App;
